289cdb4ad332b0995acd0e76effd8d78b9bdb00f
edulanarf <edulanarfrgmail.com>
2024-12-27 18:03:42 +0000

    Se crea la fabrica y el main

--- src/main/java/ulpgc/es/control/CommandFactory.java
+++ src/main/java/ulpgc/es/control/CommandFactory.java
@@ -1,4 +1,33 @@
 package ulpgc.es.control;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.lang.module.ModuleDescriptor;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CommandFactory {
+    private final Map<String, Builder> builders;
+    public CommandFactory(){
+        builders = new HashMap<>();
+    }
+
+    public void register(String name, Builder builder){
+        builders.put(name, builder);
+    }
+
+    public Selector with(HttpServletRequest req, HttpServletResponse res){
+        return name -> builders.get(name).build(req,res);
+    }
+
+    public interface Builder{
+        Command build(HttpServletRequest req, HttpServletResponse res);
+
+    }
+
+    public interface Selector{
+        Command build (String name);
+    }
 }
--- src/main/java/ulpgc/es/view/WorkingDaysService.java
+++ src/main/java/ulpgc/es/view/WorkingDaysService.java
@@ -1,6 +1,8 @@
 package ulpgc.es.view;
 
 import io.javalin.Javalin;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import ulpgc.es.control.CommandFactory;
 import ulpgc.es.control.commands.WorkingDateCommand;
 import ulpgc.es.control.commands.WorkingDaysCommand;
@@ -12,27 +14,40 @@ public class WorkingDaysService {
     private final CommandFactory factory;
     private Javalin app;
 
+
     public WorkingDaysService(int port, CommandFactory factory) {
         this.port = port;
         this.factory = factory;
-        factory.register("working-service", workingDaysBuilder());
-        factory.register("working-date", workingDaysBuilder());
+        factory.register("working-days", workingDaysBuilder());
+        factory.register("working-date", workingDateBuilder());
     }
-    public void start(){
-        app = Javalin.create().get("working-days",ctx-> execute("working-days", ctx.req(), ctx.res()))
-                .get("working-date",ctx-> execute("working-date", ctx.req(), ctx.res()))
+
+
+    public void start() {
+        app = Javalin.create()
+                .get("/working-days", ctx -> execute("working-days", ctx.req(), ctx.res()))
+                .get("/working-date", ctx -> execute("working-date", ctx.req(), ctx.res()))
                 .start(port);
-
     }
 
-    public void stop(){ app.stop();}
 
-    private static CommandFactory.Builder workingDateBuilder(){
-        return (req,res)-> new WorkingDateCommand(WorkingDateAdapter.inputOf(req), WorkingDateAdapter.outputOf(req));
+    private void execute(String command, HttpServletRequest req, HttpServletResponse res) {
+        factory.with(req, res).build(command).execute();
     }
 
-    private static CommandFactory.Builder workingDaysBuilder(){
-        WorkingDaysAdapter WorkingDaysAdapter;
-        return (req, res)-> new WorkingDaysCommand(WorkingDaysAdapter.inputOf(req), WorkingDaysAdapter.outputOf(res));
+
+    public void stop() {
+        app.stop();
+    }
+
+
+    private static CommandFactory.Builder workingDateBuilder() {
+        return (req, res) -> new WorkingDateCommand(WorkingDateAdapter.inputOf(req), WorkingDateAdapter.outputOf(res));
+    }
+
+
+    private static CommandFactory.Builder workingDaysBuilder() {
+        return (req, res) -> new WorkingDaysCommand(WorkingDaysAdapter.inputOf(req), WorkingDaysAdapter.outputOf(res));
     }
 }
+
